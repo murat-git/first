7c875ce12c7e04c64dbeaea4f800fc8bbb625e32
Murat <lmi1963@inbox.ru>
2020-04-23 22:53:01 +0600

    First commit

---
+++ app/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<div class="page-footer">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://somatic.kz/"> somatic.kz</a>
+  </div>
+  <!-- Copyright -->
+
+</div>
+<!-- Footer -->
